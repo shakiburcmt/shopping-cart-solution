f624428bce541d7c99e3c44807bc16fedd8d3bf7
shakiburcmt <shakibur.cmt@gmail.com>
2022-08-21 23:37:13 +0600

    l

--- js/phone.js
+++ js/phone.js
@@ -1,18 +1,4 @@
-function getTextElementValueById(elementId) {
-    const cost = document.getElementById(elementId);
-    const costString = cost.innerText;
-    const costTotal = parseInt(costString);
-    return costTotal;
-}
 
-function calculateSubTotal() {
-    // calculate total
-    const currentPhoneCost = getTextElementValueById('phone-total-price');
-    const currentCaseCost = getTextElementValueById('case-total-price');
-    const currentSubTotal = currentPhoneCost + currentCaseCost;
-    const subTotal = document.getElementById('sub-total');
-    subTotal.innerText = currentSubTotal;
-}
 
 // plus button add
 document.getElementById('btn-phone-plus').addEventListener('click', function () {
